a43f25287aeb50cf5bca3c9ee3c883acb694ded6
allzxxopemula <allzxxott@gmail.com>
2026-09-21 20:36:16 +0700

    almk

--- api/index.php
+++ api/index.php
@@ -1,3 +1,15 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+// Tampilkan semua error PHP
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (\Throwable $e) {
+    echo "<h1>PHP Error / Exception:</h1>";
+    echo "<p><b>Message:</b> " . $e->getMessage() . "</p>";
+    echo "<p><b>File:</b> " . $e->getFile() . " on line " . $e->getLine() . "</p>";
+    echo "<pre>" . $e->getTraceAsString() . "</pre>";
+}
